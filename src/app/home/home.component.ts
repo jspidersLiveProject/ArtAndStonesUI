1fb9c61ae1a77d1882fd15764d0613bc3a2f4205
gargvis <gargvis@192.168.0.6>
2018-05-30 09:05:47 +0530

    image clicked ngif back functionality header changes routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,23 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   images = [
-    { 'src': 'assets/images/marbel1.jpg', 'name': 'marbel1' },
-    { 'src': 'assets/images/marbel2.jpg', 'name': 'marbel2' },
-    { 'src': 'assets/images/marbel3.jpg', 'name': 'marbel3' },
-    { 'src': 'assets/images/marbel4.jpg', 'name': 'marbel4' },
-    { 'src': 'assets/images/marbel5.jpg', 'name': 'marbel5' },
-    { 'src': 'assets/images/marbel6.jpg', 'name': 'marbel6' },
-    { 'src': 'assets/images/marbel7.jpg', 'name': 'marbel7' },
-    { 'src': 'assets/images/marbel8.jpg', 'name': 'marbel8' },
-    { 'src': 'assets/images/marbel1.jpg', 'name': 'marbel1' },
-    { 'src': 'assets/images/marbel2.jpg', 'name': 'marbel2' },
-    { 'src': 'assets/images/marbel3.jpg', 'name': 'marbel3' },
-    { 'src': 'assets/images/marbel4.jpg', 'name': 'marbel4' }
-  ]
+    { src: 'assets/images/marbel1.jpg', name: 'marbel1' },
+    { src: 'assets/images/marbel2.jpg', name: 'marbel2' },
+    { src: 'assets/images/marbel3.jpg', name: 'marbel3' },
+    { src: 'assets/images/marbel4.jpg', name: 'marbel4' },
+    { src: 'assets/images/marbel5.jpg', name: 'marbel5' },
+    { src: 'assets/images/marbel6.jpg', name: 'marbel6' },
+    { src: 'assets/images/marbel7.jpg', name: 'marbel7' },
+    { src: 'assets/images/marbel8.jpg', name: 'marbel8' },
+    { src: 'assets/images/marbel1.jpg', name: 'marbel1' },
+    { src: 'assets/images/marbel2.jpg', name: 'marbel2' },
+    { src: 'assets/images/marbel3.jpg', name: 'marbel3' },
+    { src: 'assets/images/marbel4.jpg', name: 'marbel4' }
+  ];
+  gridEnabled;
+  marbelName;
+  marbelSrc;
   variable = 'marbel1';
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.gridEnabled = true;
   }
 
+  imageClicked(image) {
+    this.gridEnabled = false;
+    this.marbelSrc = image.src;
+    this.marbelName = image.name;
+  }
+
+  backClicked() {
+    this.gridEnabled = true;
+  }
 }
