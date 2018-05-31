8d2b68031af9825706fb9928097185f283627dba
gargvis <gargvis@192.168.1.10>
2018-05-31 20:07:18 +0530

    small changes for images tranparency

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,16 +14,13 @@ export class HomeComponent implements OnInit {
     { src: 'assets/images/marbel5.jpg', name: 'marbel5' },
     { src: 'assets/images/marbel6.jpg', name: 'marbel6' },
     { src: 'assets/images/marbel7.jpg', name: 'marbel7' },
-    { src: 'assets/images/marbel8.jpg', name: 'marbel8' },
-    { src: 'assets/images/marbel1.jpg', name: 'marbel1' },
-    { src: 'assets/images/marbel2.jpg', name: 'marbel2' },
-    { src: 'assets/images/marbel3.jpg', name: 'marbel3' },
-    { src: 'assets/images/marbel4.jpg', name: 'marbel4' }
+    { src: 'assets/images/marbel8.jpg', name: 'marbel8' }
   ];
   gridEnabled;
   marbelName;
   marbelSrc;
   variable = 'marbel1';
+  ifHoverOver = false;
   constructor() {}
 
   ngOnInit() {
@@ -38,5 +35,23 @@ export class HomeComponent implements OnInit {
 
   backClicked() {
     this.gridEnabled = true;
+  }
+
+  mouseEnter(image) {
+    this.ifHoverOver = true;
+    this.marbelName = image.name;
+    this.marbelSrc = image.src;
+  }
+
+  mouseLeave() {
+    this.ifHoverOver = false;
+  }
+
+  hoverOver(image) {
+    if (this.ifHoverOver && image.src === this.marbelSrc ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
